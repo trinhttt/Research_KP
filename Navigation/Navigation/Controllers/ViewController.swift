f21811db45545e2c3470756b7ff2f909a4415972
Thai Thi Tu Trinh <ttttrinh132@gmail.com>
2019-09-30 11:02:57 +0700

    update change root view & add super.loadview

--- Navigation/Navigation/Controllers/ViewController.swift
+++ Navigation/Navigation/Controllers/ViewController.swift
@@ -9,19 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // trong suốt(transparent) navigation bar
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationController?.navigationBar.shadowImage = UIImage()
-        navigationController?.navigationBar.isTranslucent = true
-
+        // navigationController?.navigationBar.isTranslucent = true
+        
         print("do viewDidLoad")
     }
     
     override func loadView() {
-        print("vang loadview")
+        super.loadView()
+        print("do loadview")
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -37,19 +38,19 @@ class ViewController: UIViewController {
     }
     override func viewDidDisappear(_ animated: Bool) {
         print("do viewDidDisappear")
-
+        
     }
-
+    
     @IBAction func ibLoginTapped(_ sender: Any) {
         
-//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-//        let vc = storyboard.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
-//        navigationController?.pushViewController(vc, animated: true)
+        //        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        //        let vc = storyboard.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+        //        navigationController?.pushViewController(vc, animated: true)
         
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let tabbar = storyboard.instantiateViewController(withIdentifier: "TabBarViewController") as! TabBarViewController
-        let appDelegate = UIApplication.shared.delegate
-        appDelegate?.window??.rootViewController = tabbar
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        appDelegate?.window?.rootViewController = tabbar
         
     }
     
